4eeb3cf1a7a72c01c5662029d80efadd7d39b94e
Ismael de la Gracia Cobos <ismael.delagracia@e.applus.com>
2026-01-09 17:05:59 +0100

    Adjuts section height

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,6 +1,6 @@
 function Section({ id, className, children }) {
     return (
-        <section id={id} className={`w-3/5 h-dvh mx-auto md:flex md:flex-row items-center ${className}`}>
+        <section id={id} className={`w-3/5 min-h-[calc(100dvh-64px)] mx-auto md:flex md:flex-row items-center ${className}`}>
             {children}
         </section>
     );
